cac791fe0e55e58f9bc28bbe65cab3397db1b5e7
Luke Lawton <llawton25@student.dimanregional.org>
2022-11-04 16:56:58 -0400

    finished it

--- Production/js/main.js
+++ Production/js/main.js
@@ -1,70 +1,12 @@
 const table_selector = $('.calendar_table_node');
 
-// the first number is for the row ||| the second is for the collumn \\
-let cell_1_1_1 = '01';  //first cell in the first row //in the first collumn //in the first table
-let cell_1_2_1 = '02';      
-let cell_1_3_1 = '03';          
-let cell_1_4_1 = '04';
-let cell_1_5_1 = '05';
-let cell_1_6_1 = '06';
-let cell_1_7_1 = '07';
-
-let cell_2_1_1 = '08';
-let cell_2_2_1 = '09';
-let cell_2_3_1 = '10';
-let cell_2_4_1 = '11';
-let cell_2_5_1 = '12';
-let cell_2_6_1 = '13';
-let cell_2_7_1 = '14';
-
-let cell_3_1_1 = '15';
-let cell_3_2_1 = '16';
-let cell_3_3_1 = '17';
-let cell_3_4_1 = '18';
-let cell_3_5_1 = '19';
-let cell_3_6_1 = '20';
-let cell_3_7_1 = '21';
-
-let cell_4_1_1 = '22';
-let cell_4_2_1 = '23';
-let cell_4_3_1 = '24';
-let cell_4_4_1 = '25';
-let cell_4_5_1 = '26';
-let cell_4_6_1 = '27';
-let cell_4_7_1 = '28';
-
-let cell_5_1_1 = '29';
-let cell_5_2_1 = '30';
-let cell_5_3_1 = '31';
-let cell_5_4_1 = '32';
-let cell_5_5_1 = '33';
-let cell_5_6_1 = '34';
-let cell_5_7_1 = '35';
-
-let cell_6_1_1 = '36';
-let cell_6_2_1 = '37';
-let cell_6_3_1 = '38';
-let cell_6_4_1 = '39';
-let cell_6_5_1 = '40';
-let cell_6_6_1 = '41';
-let cell_6_7_1 = '42';
-
-
-//each rows dummy data 1
-let dummy_row_1  = [cell_1_1_1, cell_1_2_1, cell_1_3_1, cell_1_4_1, cell_1_5_1, cell_1_6_1, cell_1_7_1];
-let dummy_row_2  = [cell_2_1_1, cell_2_2_1, cell_2_3_1, cell_2_4_1, cell_2_5_1, cell_2_6_1, cell_2_7_1];
-let dummy_row_3  = [cell_3_1_1, cell_3_2_1, cell_3_3_1, cell_3_4_1, cell_3_5_1, cell_3_6_1, cell_3_7_1];
-let dummy_row_4  = [cell_4_1_1, cell_4_2_1, cell_4_3_1, cell_4_4_1, cell_4_5_1, cell_4_6_1, cell_4_7_1];
-let dummy_row_5  = [cell_5_1_1, cell_5_2_1, cell_5_3_1, cell_5_4_1, cell_5_5_1, cell_5_6_1, cell_5_7_1];
-let dummy_row_6  = [cell_6_1_1, cell_6_2_1, cell_6_3_1, cell_6_4_1, cell_6_5_1, cell_6_6_1, cell_6_7_1];
-
-
-//the full data table
-let dummy_data_table = [dummy_row_1, dummy_row_2, dummy_row_3, dummy_row_4, dummy_row_5, dummy_row_6];  
-
-
 function fill_table(data, table) { //runs through each row and collumn, setting each cells innerHTML to the data desired
     data = data[selected_year][selected_month];
+
+    let date = new Date();                  //date variable
+    let today = date.getDate();             //get the date number in the month
+    let month = date.getMonth();            //get the month
+    let year = date.getFullYear() - 1995;   //get the year (in code the real year is set later)
 
     let table_dimensions = { //dimensions of the table
         "rows": table.rows.length - 1, //row count
@@ -78,6 +20,11 @@ function fill_table(data, table) { //runs through each row and collumn, setting 
         for (let collumn_i = 0; collumn_i < table.childNodes[1].childNodes[(row_i+1)*2].childNodes.length / 2 - 1; collumn_i++) { //runs once for each collumn within the row
             current_selected_cell = table.childNodes[1].childNodes[(row_i+1)*2].childNodes[collumn_i * 2 + 1]; //sets the selected cell for easy manipulation
             current_selected_cell.innerHTML = data[row_i][collumn_i]; //sets the innerhtml to the data passed
+            if (current_selected_cell.innerHTML == today && selected_month == month && selected_year == year) { //when the selected cell is the current day
+                current_selected_cell.style.backgroundColor = 'var(--calendar-secondary)' //highlights the cell
+            } else {
+                current_selected_cell.style.backgroundColor = ''; //unhighlights the cell when its no longer the same
+            }
         }
     }
 
@@ -95,17 +42,17 @@ function $(a) { //$ function query selector
 //need to add function for leap years, and the 400 year gregorian rule
 function get_calendar_data(year_count) { //starting year 1995 cuz the first was on a monday
     const month_day_count = [
-        31, 28, 30, 30,
+        31, 28, 31, 30,
         31, 30, 31, 31,
         30, 31, 30, 31
     ];                                                                          //data on the amount of days in a month
     let years = [];                                                             //every year generated
     let leap_index = 2;                                                         //sets the leap index according to the first year (1995) hardcoded
     let leap_day = 0;                                                           //initializes the leap day to 0
-
+    let stop_index = 0;                                                         //index on what place the code stopped counting up
+    
     for (let i = 0; i < year_count; i++) { //once for each year
         let current_year = [];                                                  //the year that the loop generates
-        let stop_index = 0;                                                     //index on what place the code stopped counting up
         leap_index++;                                                           //increments the leap index up one
         
         for (let month_i = 0; month_i < 12; month_i++) { //once for each month
@@ -138,7 +85,6 @@ function get_calendar_data(year_count) { //starting year 1995 cuz the first was 
         // console.log(current_year);
         years.push(current_year);                                               //pushes the year to years array
     }
-    console.log('done');
     return years;
 }
 
@@ -147,8 +93,12 @@ let selected_year = 0; //1995
 
 function change_month(e) {
     if (selected_year == 0 && isNegative(e) && isNegative(selected_month-1)) return;    //prevents from going bellow 0
-    if (e==-12 && selected_year == 0) return selected_month = 0;                        //prevents from going bellow 0
-    if (selected_year == 2500 - 1995 && e==12 || selected_year == 2500 - 1995 && selected_month>10 && e==1) return; //prevents from going bellow 0
+    if (e==-12 && selected_year == 0) {                                                 //prevents from going bellow 0
+        selected_month = 0;
+        fill_table(calendar_data, table_selector);
+        return;
+    }
+    if (selected_year == 2500 - 1995 && e==12 || selected_year == 2500 - 1995 && selected_month>10 && e==1) return; //prevents from going above 2500
 
     if(e==1)  selected_month++; //increments up one month
     if(e==-1) selected_month--; //increments down one month
@@ -174,12 +124,12 @@ function change_month(e) {
 let calendar_data;
 window.onload = (e) => {
     let date = new Date(); //date variable
-
+    
     selected_year = date.getFullYear() - 1995;  //sets the year
     selected_month = date.getMonth();           //sets the month
-
-    calendar_data = get_calendar_data(2500 - 1994);   //sets calendar_data
-    fill_table(calendar_data, table_selector);              //fills the table
+    
+    calendar_data = get_calendar_data(2500 - 1994);     //sets data for the calendar to set to
+    fill_table(calendar_data, table_selector);          //fills the table
 }
 
 function isNegative(num) {
